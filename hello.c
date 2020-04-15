594926b5656f21c86491299eabd62447061b7250
문승재 <gaeddongie13@gmail.com>
2020-04-15 19:38:32 +0900

    add error C code

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h> 
-int main () { 
-   printf ( “Hello world”); 
-   return 0;
+
+int sum(int c, int b) {
+	int c = a + b;
+	return c;
+}
+
+int main() {
+	printf("두 수의 합은 = %f\n", sum(1, 1));
+	return 0;
 }
