9e3bad1e4b1b1a1f39034edf9f219b3d6d5b3b22
문승재 <gaeddongie13@gmail.com>
2020-05-20 21:30:16 +0900

    진교찬 고생했다

--- 진교찬/djangoproject/firstproject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-import wordcount.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',wordcount.views.home, name="home"),
-    path('about/',wordcount.views.about, name="about"),
-]
