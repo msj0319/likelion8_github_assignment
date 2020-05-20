165e7cdfb733ef15383b8e45f1e74de2ae526c78
Jin Gyo Chan <jinkai4868@likelion.org>
2020-05-20 21:15:07 +0900

    T1

--- 이재현/week4/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	  $("#thanos").click(function(){
-	    $("#ironman").fadeToggle();
-	    $("#hulk").fadeToggle("slow");
-	    $("#groot").fadeToggle(3000);
-	  });
-	});
