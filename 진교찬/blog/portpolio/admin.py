f2ce1c9c6bd183340d1041d777eb519bcc92cff5
Jin Gyo Chan <jinkai4868@likelion.org>
2020-06-03 14:51:04 +0900

    blog

---
+++ 진교찬/blog/portpolio/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Portpolio
+
+admin.site.register(Portpolio)
+# Register your models here.
---
+++ 진교찬/blog/portpolio/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Portpolio
+# Create your views here.
+def portpolio(request):
+    portpolios = Portpolio.objects
+    return render(request, 'portpolio.html', {'portpolios':portpolios})
